948e2b6174e14a81bf3492ca6b2d9e8ea5bd85f6
ecommercelife <54946804+ecommercelife@users.noreply.github.com>
2026-06-17 22:11:15 -0300

    Add API handler for fetching product costs

---
+++ api/custos.js
@@ -0,0 +1,50 @@
+async function getAccessToken() {
+  const params = new URLSearchParams();
+
+  params.append("grant_type", "refresh_token");
+  params.append("client_id", process.env.TINY_CLIENT_ID);
+  params.append("client_secret", process.env.TINY_CLIENT_SECRET);
+  params.append("refresh_token", process.env.TINY_REFRESH_TOKEN);
+
+  const response = await fetch(
+    "https://accounts.tiny.com.br/realms/tiny/protocol/openid-connect/token",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: params,
+    }
+  );
+
+  const data = await response.json();
+
+  return data.access_token;
+}
+
+export default async function handler(req, res) {
+  try {
+
+    const token = await getAccessToken();
+
+    const response = await fetch(
+      "https://api.tiny.com.br/public-api/v3/produtos/345335450/custos",
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    const data = await response.json();
+
+    return res.status(200).json(data);
+
+  } catch (error) {
+
+    return res.status(500).json({
+      erro: error.message
+    });
+
+  }
+}
