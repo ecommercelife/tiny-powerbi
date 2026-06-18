f6d26bc08a7b45a914cac9328c3371dd2a34f54d
ecommercelife <54946804+ecommercelife@users.noreply.github.com>
2026-06-17 21:15:05 -0300

    Implement OAuth2 token retrieval in callback handler
    Refactor handler to process authorization code and fetch token.

--- api/callback.js
+++ api/callback.js
@@ -1,6 +1,29 @@
 export default async function handler(req, res) {
-  return res.status(200).json({
-    clientIdExiste: !!process.env.TINY_CLIENT_ID,
-    clientSecretExiste: !!process.env.TINY_CLIENT_SECRET,
-  });
+  const { code } = req.query;
+
+  const basic = Buffer.from(
+    `${process.env.TINY_CLIENT_ID}:${process.env.TINY_CLIENT_SECRET}`
+  ).toString("base64");
+
+  const params = new URLSearchParams();
+
+  params.append("grant_type", "authorization_code");
+  params.append("redirect_uri", "https://tiny-powerbi.vercel.app/api/callback");
+  params.append("code", code);
+
+  const response = await fetch(
+    "https://accounts.tiny.com.br/realms/tiny/protocol/openid-connect/token",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization: `Basic ${basic}`
+      },
+      body: params
+    }
+  );
+
+  const data = await response.json();
+
+  return res.status(response.status).json(data);
 }
