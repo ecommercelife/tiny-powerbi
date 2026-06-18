eb6604a8a6e93e70daace18615a9d7c1da9cf7be
ecommercelife <54946804+ecommercelife@users.noreply.github.com>
2026-06-17 21:13:18 -0300

    Refactor callback handler to check client credentials
    Removed code that processes authorization code and added response to check for environment variables.

--- api/callback.js
+++ api/callback.js
@@ -1,35 +1,6 @@
 export default async function handler(req, res) {
-  const { code } = req.query;
-
-  if (!code) {
-    return res.status(400).json({
-      erro: "Código não informado"
-    });
-  }
-
-  const params = new URLSearchParams();
-
-  params.append("grant_type", "authorization_code");
-  params.append("client_id", process.env.TINY_CLIENT_ID);
-  params.append("client_secret", process.env.TINY_CLIENT_SECRET);
-  params.append(
-    "redirect_uri",
-    "https://tiny-powerbi.vercel.app/api/callback"
-  );
-  params.append("code", code);
-
-  const response = await fetch(
-    "https://accounts.tiny.com.br/realms/tiny/protocol/openid-connect/token",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded"
-      },
-      body: params
-    }
-  );
-
-  const data = await response.json();
-
-  return res.status(200).json(data);
+  return res.status(200).json({
+    clientIdExiste: !!process.env.TINY_CLIENT_ID,
+    clientSecretExiste: !!process.env.TINY_CLIENT_SECRET,
+  });
 }
