2300f88469aa8b6b38e72852043d2d1a5ced8de6
ecommercelife <54946804+ecommercelife@users.noreply.github.com>
2026-06-17 21:08:48 -0300

    Update callback.js

--- api/callback.js
+++ api/callback.js
@@ -1,7 +1,14 @@
 export default async function handler(req, res) {
+  const { code } = req.query;
+
+  if (!code) {
+    return res.status(400).json({
+      erro: "Código não informado"
+    });
+  }
+
   return res.status(200).json({
-    success: true,
-    message: "Callback funcionando",
-    query: req.query
+    sucesso: true,
+    code
   });
 }
