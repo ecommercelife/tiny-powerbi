85c7dc2617b41168947154615a9f45ab4289d40e
ecommercelife <54946804+ecommercelife@users.noreply.github.com>
2026-06-17 21:10:12 -0300

    Update callback function to fetch token
    Refactor callback function to handle authorization code and fetch token from Tiny API.

--- api/callback.js
+++ api/callback.js
@@ -7,8 +7,29 @@ export default async function handler(req, res) {
     });
   }
 
-  return res.status(200).json({
-    sucesso: true,
-    code
-  });
+  const params = new URLSearchParams();
+
+  params.append("grant_type", "authorization_code");
+  params.append("client_id", process.env.TINY_CLIENT_ID);
+  params.append("client_secret", process.env.TINY_CLIENT_SECRET);
+  params.append(
+    "redirect_uri",
+    "https://tiny-powerbi.vercel.app/api/callback"
+  );
+  params.append("code", code);
+
+  const response = await fetch(
+    "https://accounts.tiny.com.br/realms/tiny/protocol/openid-connect/token",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      body: params
+    }
+  );
+
+  const data = await response.json();
+
+  return res.status(200).json(data);
 }
